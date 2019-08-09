eef7dd569e3efce1afd9759394108db754c999ff
cjswlcjrtk <cjswlcjrtk@naver.com>
2019-08-09 18:13:08 +0900

    190809 18:13

--- java-basic/WhatName.java
+++ java-basic/WhatName.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 class WhatName{
     public static void main(String[] args){
 
         
-        System.out.print("What's your name?'");
-        System.out.print("My Name is " + name);
+        System.out.println("이름 뭐니?");
+        String name = "";
+        int age = 0;
+        // 값을 입력받는 지점
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.next();
+
+        System.out.println("나이 뭐니?");
+        age = scanner.nextInt();
+        // System.out.print("이름은 " + name + "이다");
+        System.out.print("이름은 " + name + ", 이고\n 나이는 " + age + "살 이다.");
     }
     
 
